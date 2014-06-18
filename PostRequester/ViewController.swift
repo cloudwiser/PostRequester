3d05ecaf32b67fb0d24076a47ad5544dbab264c6
Nick <nickhall@mac-mini.home>
2014-06-19 00:00:06 +0100

    Initial Commit

---
+++ PostRequester/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PostRequester
+//
+//  Created by Nick on 19/06/2014.
+//  Copyright (c) 2014 cloudwise. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
